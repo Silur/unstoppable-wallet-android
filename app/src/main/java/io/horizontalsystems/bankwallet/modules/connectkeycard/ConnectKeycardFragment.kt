8fe0a222426fbaf4d8628c7126931c23851bc27e
Silur <abrahamendre9@gmail.com>
2021-07-23 20:37:41 +0200

    create add keycard fragment

---
+++ app/src/main/java/io/horizontalsystems/bankwallet/modules/connectkeycard/ConnectKeycardFragment.kt
@@ -0,0 +1,7 @@
+package io.horizontalsystems.bankwallet.modules.connectkeycard
+
+import io.horizontalsystems.bankwallet.core.BaseFragment
+
+class ConnectKeycardFragment : BaseFragment() {
+    
+}
